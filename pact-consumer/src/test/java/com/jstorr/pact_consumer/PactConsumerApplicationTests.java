faf18921ca79ff766102e0886242288deee2e693
James Storr <jamesstorr@live.co.uk>
2025-01-13 20:10:29 +0000

    Initial commit

---
+++ pact-consumer/src/test/java/com/jstorr/pact_consumer/PactConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstorr.pact_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
