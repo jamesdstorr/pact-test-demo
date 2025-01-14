669c920ae5ed9f7a3a5091a9a518a93e9ef139a9
James Storr <jamesstorr@live.co.uk>
2025-01-14 20:52:47 +0000

    fixed tests
    fixed tests

--- pact-provider/src/test/java/com/jstorr/pact_provider/contract/PactProviderTest.java
+++ pact-provider/src/test/java/com/jstorr/pact_provider/contract/PactProviderTest.java
@@ -1,19 +1,44 @@
 package com.jstorr.pact_provider.contract;
 
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @Provider("UserProvider")
-@PactBroker
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ProductPactProviderTest {
+@PactBroker(
+        host = "localhost",
+        port = "9292"
+)
+class PactProviderTest {
+
+    @Value("${local.server.port}")
+    private int serverPort;
+    private HttpTestTarget target = new HttpTestTarget("localhost", serverPort);
+
+
+
+    @State("The provider can create a user") // Corrected state
+    public void setupCreateUser() {
+        // Mock or prepare backend data for creating a user
+        System.out.println("Setting up provider state: the provider can create a user");
+    }
+
+    @State("A list of users exists") // Corrected state
+    public void setupListUsers() {
+        // Mock or prepare backend data for fetching a list of users
+        System.out.println("Setting up provider state: a list of users exists");
+    }
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
@@ -21,4 +46,8 @@ class ProductPactProviderTest {
         context.verifyInteraction();
     }
 
+    @BeforeEach
+    void setTarget(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", serverPort));
+    }
 }
